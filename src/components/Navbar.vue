28c91f3724d892c95691ad52237bd7f2dc49a1de
Miens332 <alejandrorandon@gmail.com>
2020-01-21 11:29:12 -0400

    commit 1

---
+++ src/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav class="flex justify-between bg-gray-200 p-2">
+    <img src="../assets/school.png" width="35px" class="ml-2">
+    <img src="../assets/menu-alt-512.png" width="35px" class="mr-2">
+  </nav>
+
+</template>
